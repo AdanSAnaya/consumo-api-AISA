a9fa62c2bd395aa6a22d244d93ea926b974450a1
ADAN ISMAEL SANTIAGO ANAYA <20161341@itoaxaca.edu.mx>
2024-11-14 19:41:19 -0600

    Cambios de Paginación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserService } from './services/user.service';
 import { UserListComponent } from './components/user-list/user-list.component';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,10 +19,15 @@ import { UserListComponent } from './components/user-list/user-list.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule, 
+    MatPaginatorModule, 
+    MatButtonModule, 
+    MatIconModule
   ],
-  providers: [[UserService],],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
 @Component({
-selector: 'app-user-list',
-templateUrl: './user-list.component.html',
-styleUrls: ['./user-list.component.css']
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
 })
-  export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit {
   users: any[] = [];
+  displayedColumns: string[] = ['id', 'name', 'email', 'role'];
+  dataSource = new MatTableDataSource<any>(this.users);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private userService: UserService) {}
+
   ngOnInit(): void {
-  this.userService.getUsers().subscribe(data => {
-  this.users = data;
-  });
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+      this.dataSource.data = this.users;
+      this.dataSource.paginator = this.paginator;
+    });
   }
 }
